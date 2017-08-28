022d42a29d945288bbf3b935b1390efb3b966e03
ReinaSakuraba <juniorterra7@gmail.com>
2017-08-28 13:21:00 -0700

    Update baut.py

--- baut.py
+++ baut.py
@@ -1,70 +1,60 @@
-from discord.ext import commands
-import asyncio, discord, io, textwrap, traceback
+import asyncio
+from pathlib import Path
 from itertools import cycle
-from contextlib import redirect_stdout
-bot = commands.Bot(command_prefix=lambda b,m: m.author.name[0], game=discord.Game(name="yes"))
-bot._last_result = None
-async def cyc():
-    await bot.wait_until_ready()
-    guild = bot.get_guild(328873861481365514)
-    for member in cycle(guild.members):
-        await guild.me.edit(nick=member.name.upper())
-        await asyncio.sleep(5)
-def cleanup_code(content):
-    if content.startswith('```') and content.endswith('```'):
-        return '\n'.join(content.split('\n')[1:-1])
-    return content.strip('` \n')
-def get_syntax_error(e):
-    if e.text is None:
-        return f'```py\n{e.__class__.__name__}: {e}\n```'
-    return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
-@bot.command(name='eval')
-async def _eval(ctx, *, body: str):
-    """Evaluates code."""
-    env = {
-        'bot': ctx.bot,
-        'ctx': ctx,
-        'channel': ctx.channel,
-        'author': ctx.author,
-        'guild': ctx.guild,
-        'message': ctx.message,
-        '_': bot._last_result
-    }
 
-    env.update(globals())
+import aiohttp
+import discord
+from discord.ext import commands
 
-    body = cleanup_code(body)
-    stdout = io.StringIO()
 
-    code = textwrap.indent(body, '  ')
-    to_compile = f'async def func():\n{code}'
+class Context(commands.Context):
+    @property
+    def session(self):
+        return self.bot.session
 
-    try:
-        exec(to_compile, env)
-    except SyntaxError as e:
-        return await ctx.send(get_syntax_error(e))
 
-    func = env['func']
-    try:
-        with redirect_stdout(stdout):
-            ret = await func()
-    except Exception as e:
-        value = stdout.getvalue()
-        await ctx.send(f'​`​`​`py\n{value}{traceback.format_exc()}\n​`​`​`')
-    else:
-        value = stdout.getvalue()
-        try:
-            await ctx.message.add_reaction('\u2705')
-        except:
-            pass
+class Bot(commands.Bot):
+    def __init__(self):
+        def get_prefix(bot, message):
+            return message.author.name[0]
 
-        if ret is None:
-            if value:
-                await ctx.send(f'```py\n{value}\n```')
-        else:
-            bot._last_result = ret
-            await ctx.send(f'```py\n{value}{ret}\n```')
-async def on_ready():
-    print("yes")
-bot.loop.create_task(cyc())
-bot.run("MjU0NjE1MTA4NTE5NDYwODY1.DIWGmw.BDtt1fYwK0Bx5U0BAwmqdSYZ9aA")
+        super().__init__(command_prefix=get_prefix,
+                         game=discord.Game(name="yes"))
+
+        self.session = aiohttp.ClientSession(loop=self.loop)
+
+        startup_extensions = [x.stem for x in Path('cogs').glob('*.py')]
+        for extension in startup_extensions:
+            try:
+                self.load_extension(f'cogs.{extension}')
+            except Exception as e:
+                error = f'{extension}\n {type(e).__name__}: {e}'
+                print(f'Failed to load extension {error}')
+
+        self.loop.create_task(cyc())
+
+    def _do_cleanup(self):
+        self.session.close()
+        super()._do_cleanup()
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+        print('-------------')
+
+    async def on_message(self, message):
+        ctx = await self.get_context(message, cls=Context)
+        if ctx.prefix is not None:
+            ctx.command = self.all_commands.get(ctx.invoked_with.lower())
+            await self.invoke(ctx)
+
+    async def cyc(self):
+        await self.wait_until_ready()
+        guild = self.get_guild(328873861481365514)
+        for member in cycle(guild.members):
+            await guild.me.edit(nick=member.name.upper())
+            await asyncio.sleep(5)
+
+
+if __name__ == '__main__':
+    bot = Bot()
+    bot.run("MjU0NjE1MTA4NTE5NDYwODY1.DIWGmw.BDtt1fYwK0Bx5U0BAwmqdSYZ9aA")
