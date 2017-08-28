4efbac8d46e42ca435aff19b44ce149617fb98d3
Synder <danny@will-never-love.me>
2017-08-28 23:28:19 +0300

    add admin.py

---
+++ cogs/admin.py
@@ -0,0 +1,76 @@
+import io
+import textwrap
+import traceback
+import asyncio
+import discord
+from discord.ext import commands
+from itertools import cycle
+from contextlib import redirect_stdout
+
+
+class Admin:
+    def __init__(self, bot):
+        self.bot = bot
+
+    
+    def cleanup_code(self, content):
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+        return content.strip('` \n')
+
+    def get_syntax_error(self, e):
+        if e.text is None:
+            return f'```py\n{e.__class__.__name__}: {e}\n```'
+        return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
+
+
+    @commands.command(name='eval')
+    async def _eval(self, ctx, *, body: str):
+        """Evaluates code."""
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self.bot._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        code = textwrap.indent(body, '  ')
+        to_compile = f'async def func():\n{code}'
+
+        try:
+            exec(to_compile, env)
+        except SyntaxError as e:
+            return await ctx.send(self.get_syntax_error(e))
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'​`​`​`py\n{value}{traceback.format_exc()}\n​`​`​`')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self.bot._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
