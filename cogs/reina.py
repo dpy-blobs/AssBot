a47f7809eefadeaaa80dfcdf0b807d6300f71d73
ReinaSakuraba <juniorterra7@gmail.com>
2017-08-31 12:46:50 -0700

    Update command uptime to use bot uptime attribute

--- cogs/reina.py
+++ cogs/reina.py
@@ -1,41 +1,10 @@
 import io
 import inspect
 import textwrap
-import datetime
 
 import psutil
 import discord
 from discord.ext import commands
-
-
-def human_time(seconds):
-    seconds = int(seconds)
-    if seconds == 0:
-        return '0 seconds'
-
-    hours, remainder = divmod(seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    days, hours = divmod(hours, 24)
-    years, days = divmod(days, 365)
-
-    time_units = {
-        'year': years,
-        'day': days,
-        'hour': hours,
-        'minute': minutes,
-        'second': seconds
-    }
-
-    def _plural(name, value):
-        if value != 1:
-            name += 's'
-        return f'{value} {name}'
-
-    time = [_plural(key, value) for key, value in time_units.items() if value]
-
-    if len(time) > 2:
-        return f'{", ".join(time[:-1])}, and {time[-1]}'
-    return ' and '.join(time)
 
 
 class Reina:
@@ -46,8 +15,7 @@ class Reina:
     async def uptime(self, ctx):
         """Shows the bot's uptime."""
 
-        delta = datetime.datetime.utcnow() - ctx.bot.start_time
-        await ctx.send(f'Uptime: **{human_time(delta.total_seconds()}**.
+        await ctx.send(f'Uptime: **{ctx.bot.uptime}**.
 
     @commands.command()
     async def memory(self, ctx):
