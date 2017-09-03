6fbf9e29c239fb5d1deb205a9d61647853e427e6
ReinaSakuraba <juniorterra7@gmail.com>
2017-09-02 21:35:57 -0700

    Move eval to seperate file

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,12 +1,7 @@
-import io
 import os
 import json
-import textwrap
-import traceback
 import threading
 import asyncio
-
-from contextlib import redirect_stdout
 
 import discord
 from discord.ext import commands
@@ -15,7 +10,6 @@ from discord.ext import commands
 class Admin:
     def __init__(self, bot):
         self.bot = bot
-        self.bot._last_result = None
 
     async def __local_check(self, ctx):
         role = discord.utils.get(ctx.guild.roles, id=352849291733237771)
@@ -35,16 +29,6 @@ class Admin:
                     await ctx.send('Avatar set.')
             else:
                 await ctx.send('Unable to download image.')
-
-    def cleanup_code(self, content):
-        if content.startswith('```') and content.endswith('```'):
-            return '\n'.join(content.split('\n')[1:-1])
-        return content.strip('` \n')
-
-    def get_syntax_error(self, e):
-        if e.text is None:
-            return f'```py\n{e.__class__.__name__}: {e}\n```'
-        return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
 
     @commands.command()
     async def load(self, ctx, *, module: str):
@@ -82,54 +66,6 @@ class Admin:
             await ctx.send(f'{type(e).__name__}: {e}')
         else:
             await ctx.send('\N{OK HAND SIGN}')
-
-    @commands.command(name='eval')
-    async def _eval(self, ctx, *, body: str):
-        """Evaluates code."""
-        env = {
-            'bot': self.bot,
-            'ctx': ctx,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            'guild': ctx.guild,
-            'message': ctx.message,
-            '_': self.bot._last_result,
-            'kkk': 'Racist!'
-        }
-
-        env.update(globals())
-
-        body = self.cleanup_code(body)
-        stdout = io.StringIO()
-
-        code = textwrap.indent(body, '  ')
-        to_compile = f'async def func():\n{code}'
-
-        try:
-            exec(to_compile, env)
-        except SyntaxError as e:
-            return await ctx.send(self.get_syntax_error(e))
-
-        func = env['func']
-        try:
-            with redirect_stdout(stdout):
-                ret = await func()
-        except Exception as e:
-            value = stdout.getvalue()
-            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-        else:
-            value = stdout.getvalue()
-            try:
-                await ctx.message.add_reaction('🍡')
-            except:
-                pass
-
-            if ret is None:
-                if value:
-                    await ctx.send(f'```py\n{value}\n```')
-            else:
-                self.bot._last_result = ret
-                await ctx.send(f'```py\n{value}{ret}\n```')
 
     @commands.command()
     async def gitmerge(self, ctx, *pr_numbers):
