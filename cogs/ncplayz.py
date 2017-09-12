72b2b12031cddf7679311e419c595281fa6d8aa3
Nadir Chowdhury <NCPlayz@users.noreply.github.com>
2017-09-12 22:11:20 +0100

    Update ncplayz.py

--- cogs/ncplayz.py
+++ cogs/ncplayz.py
@@ -28,10 +28,13 @@ class NCPlayz:
     async def role(self, ctx, *, role: discord.Role):
         """Fetches the ID of a specified role"""
         if role:
-            msg = "The id of the role `{0}` is `{0.id}".format(role)
-        else: 
-            msg = "**Error!** A role named {0} could not be found! You must enter the exact name (including caps).".format(request)
+             msg = "The id of the role `{0}` is `{0.id}".format(role)
         await ctx.send(msg)
+        
+    @role.error()
+    async def error_role(self, ctx):
+        if isinstance(exception, commands.BadArgument):
+            await ctx.send("There was an error when performing that action.")
     
     @ID.command()
     async def member(self, ctx, *, member: discord.Member = None):
@@ -39,10 +42,14 @@ class NCPlayz:
         member = member or ctx.author
         if member:
             msg = "The id of the member `{0.name}` is `{0.id}".format(member)
-        else:
-            msg = "**Error!** A member named {0} could not be found! You must enter the exact name (including caps).".format(request)
         await ctx.send(msg)
+      
 
+    @member.error()
+    async def error_member(self, ctx):
+        if isinstance(exception, commands.BadArgument):
+            await ctx.send("There was an error when performing that action.")
+            
     @ID.command()
     async def server(self, ctx):
         """Fetches the ID of the server """
