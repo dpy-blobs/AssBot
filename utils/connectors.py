4d76aff31fa5f3b915551a81331926fb3022f5f7
MysterialPy <mysterialpy@gmail.com>
2017-09-20 16:59:33 +1000

    Update fetcherino
    Made it more dynamic, it now accepts any valid aiohttp request kwargs associated with method GET.
    Added: headers kwarg
    Changed: body is now return_type to represent the function properly.

--- utils/connectors.py
+++ utils/connectors.py
@@ -1,8 +1,11 @@
 from cogs.error import ResponseStatusError
 
-async def fetch(session, url: str, timeout: float = None, raise_over: int = 300, body: str = 'json'):
-    async with session.get(url, timeout=timeout) as resp:
-        if resp.status >= raise_over:
+async def fetch(session, url: str, headers: dict=None, timeout: float=None,
+                raise_over: int=300, return_type: str='json', **kwargs):
+    """A simple and dynamic way to fetch with aiohttp."""
+
+    async with session.get(url, headers=headers, timeout=timeout, **kwargs) as resp:
+        if resp.status > raise_over:
             raise ResponseStatusError(resp.status, resp.reason, url)
-        cont = getattr(resp, body)
+        cont = getattr(resp, return_type)
         return await cont()
